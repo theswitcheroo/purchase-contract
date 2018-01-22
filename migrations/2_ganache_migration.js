95831c7b5dc099c4d3bba008e8d61647e54a8220
Nick Neuman <nick.m.neuman@gmail.com>
2018-01-22 08:46:15 -0600

    Migration to Ganache now working for PurchaseCreator contract.

--- migrations/2_ganache_migration.js
+++ migrations/2_ganache_migration.js
@@ -2,6 +2,6 @@ var PurchaseCreator = artifacts.require("./PurchaseCreator.sol");
 var SimplePurchase = artifacts.require("./SimplePurchase.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(PurchaseCreator, {from: 0x627306090abaB3A6e1400e9345bC60c78a8BEf57});
-    deployer.deploy(SimplePurchase);
+    deployer.deploy(PurchaseCreator);
+    //deployer.deploy(SimplePurchase);
 }

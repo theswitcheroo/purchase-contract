5eec0ce1d652caee729a8c60e91a25ee73552e51
Nick Neuman <nick.m.neuman@gmail.com>
2018-01-29 09:05:22 -0600

    Writing tests for PurchaseCreator

--- test/PurchaseCreatorTest.js
+++ test/PurchaseCreatorTest.js
@@ -14,12 +14,45 @@ var SimplePurchase = artifacts.require("SimplePurchase");
 
 
 contract('PurchaseCreator', function(owner) {
+    var seller = accounts[1];
+    var switcheroo = accounts[0];
+
     it("should increment PurchaseId", function() {
+        var factory;
+        var priorPurchaseId;
+        var PurchaseId;
+
+        return PurchaseCreator.deployed().then(function(instance) {
+            factory = instance;
+            return factory.nextPurchaseId.then(function(instance) {
+                priorPurchaseId = instance;
+            });
+            return factory.newSimplePurchaseContract(seller, 5).then(function(ret1, ret2) {
+                PurchaseId = ret2;
+                assert.equal(PurchaseId, priorPurchaseId + 1, "Did not increment properly")
+            });
+        });
+    });
+
+    it("should create new Simple Purchase", function() {
+        var factory;
+        var newPurch;
+
+        return PurchaseCreator.deployed().then(function(instance) {
+            factory = instance;
+            return factory.newSimplePurchaseContract(seller, 5).then(function(ret1, ret2) {
+                newPurch = ret1;
+                //TODO assert that the new contract was deployed correctly
+    })
+
+    it("should have Switcheroo as owner", function() {
         var factory;
 
         return PurchaseCreator.deployed().then(function(instance) {
             factory = instance;
-            return factory.newSimplePurchaseContract.
+            return factory.owner.then(function(owner) {
+                assert.equal(owner.address, switcheroo, "Switcheroo is not the owner");
+            });
         });
     });
 });
